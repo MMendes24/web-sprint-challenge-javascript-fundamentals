b5ebdaa81ab182a4a163a73dee0b9710e32e7605
MMendes24 <mmbusiness1@protonmail.com>
2020-06-19 11:16:56 -0400

    Closure page completed. On to interview questions.

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,20 @@ myFunction();
 
 // Explanation: 
 
+//It's lexical scope is internal, meaning it has access to its own variables, the variables of the functions outside of it, and the global scope, but it would not have access to variables nested within it (if they present.)
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(number){
+  let counter = 0
+  let number2 = number
+  for(let i = 0; i < number2; i++){
+    counter = number + counter;
+    number--
+  }
+  return counter;
+}
+summation(4)
+
